87c310a4aaf9068b38cccb61a209a17e3850b591
Sergio <sergioljr@gmail.com>
2019-03-19 01:47:48 -0300

    Simulando erro

--- TestePortas/Program.cs
+++ TestePortas/Program.cs
@@ -22,16 +22,16 @@ namespace TestePortas
 
             //ServicePointManager.DefaultConnectionLimit = 2;
             //ServicePointManager.DnsRefreshTimeout = 10000;
-            //ServicePointManager.EnableDnsRoundRobin = true;
+            ServicePointManager.EnableDnsRoundRobin = true;
             //ServicePointManager.MaxServicePointIdleTime = 1;
-            //ServicePointManager.ReusePort = false;
+            ServicePointManager.ReusePort = true;
             //ServicePointManager.MaxServicePoints = 1;
 
 
-            HttpClient Client = new HttpClient();
+            //HttpClient Client = new HttpClient();
 
 
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 10000; i++)
             {
                 lista.Add(string.Format("Chamada {0}", i));
             }
@@ -40,37 +40,131 @@ namespace TestePortas
             try
             {
 
+                bool endProcess = false;
+                bool showExp = false;
                 var exceptions = new ConcurrentQueue<Exception>();
 
-                //Parallel.ForEach(lista, new ParallelOptions { MaxDegreeOfParallelism = 300 }, async (currentFile) =>
+                //using (HttpClient Client = new HttpClient())
+
+                //for (int i = 0; i < 200; i++)
+                { 
+                    //Parallel.ForEach(lista, new ParallelOptions { MaxDegreeOfParallelism = 300 }, async (currentFile) =>
                 Parallel.ForEach(lista, async (currentFile) =>
                 {
-
-                    Console.WriteLine($"Processing {currentFile} on thread {Thread.CurrentThread.ManagedThreadId}");
+                    
+                    
 
                     try
                     {
-                        var result = Client.GetAsync("https://taskerswift.azurewebsites.net/openapi").Result;
-                        if (result.IsSuccessStatusCode)
-                            result = Client.GetAsync("https://fakerestapi.azurewebsites.net/api/Books").Result;
-                        if (result.IsSuccessStatusCode)
-                            result = Client.GetAsync("http://andrewgodfroyportfolioapi.azurewebsites.net/api/LinkType").Result;
+                        if (endProcess)
+                        {
 
-                        throw new Exception("errro");
+                            if ((exceptions.Count > 0) && (!showExp))
+                            {
+                                Console.WriteLine($"Finished {currentFile} on thread {Thread.CurrentThread.ManagedThreadId}");
+                                Console.WriteLine(exceptions.ToArray()[0].ToString());
+                                showExp = true;
+                            }
+                            else
+                            {
 
-                        Console.WriteLine($"Result {result.StatusCode} on thread {Thread.CurrentThread.ManagedThreadId}");
+                            }
+
+                        }
+                        else
+                        {
+                            //ServicePointManager.ReusePort = false;
+                            //ServicePointManager.DnsRefreshTimeout = 100;
+                            //ServicePointManager.SetTcpKeepAlive(false, 100, 10);
+
+                            for (int i = 0; i < 200; i++)
+                            using (HttpClient Client = new HttpClient())
+                            {
+                                Console.WriteLine($"Processing {currentFile} on thread {Thread.CurrentThread.ManagedThreadId}");
+
+                                var result = Client.GetAsync("http://taskerswift.azurewebsites.net/openapi").Result;
+                                if (result.IsSuccessStatusCode)
+                                    result = Client.GetAsync("http://fakerestapi.azurewebsites.net/api/Books").Result;
+                                if (result.IsSuccessStatusCode)
+                                    result = Client.GetAsync("http://andrewgodfroyportfolioapi.azurewebsites.net/api/LinkType").Result;
+                                if (result.IsSuccessStatusCode)
+                                    Client.GetAsync("https://graph.facebook.com");
+
+                                var res = Client.GetAsync("https://www.api-football.com/demo/").Result;
+                                var res2 = Client.GetAsync("http://slowwly.robertomurray.co.uk/delay/8000/url/https://www.api-football.com/demo/").Result;
+                                var res3 = Client.GetAsync("http://slowwly.robertomurray.co.uk/delay/3000/url/https://graph.facebook.com/").Result;
+
+                                //throw new Exception("errro");
+
+                                //Console.WriteLine($"Result {result.StatusCode} on thread {Thread.CurrentThread.ManagedThreadId}");
+                            }
+
+                            //ServicePointManager.ReusePort = false;
+                            using (HttpClient Client = new HttpClient())
+                            {
+                                Console.WriteLine($"Processing {currentFile} on thread {Thread.CurrentThread.ManagedThreadId}");
+
+                                var result = Client.GetAsync("http://taskerswift.azurewebsites.net/openapi").Result;
+                                if (result.IsSuccessStatusCode)
+                                    result = Client.GetAsync("http://fakerestapi.azurewebsites.net/api/Books").Result;
+                                if (result.IsSuccessStatusCode)
+                                    result = Client.GetAsync("http://andrewgodfroyportfolioapi.azurewebsites.net/api/LinkType").Result;
+                                if (result.IsSuccessStatusCode)
+                                    Client.GetAsync("https://graph.facebook.com");
+
+                                var res = Client.GetAsync("https://www.api-football.com/demo/").Result;
+                                var res2 = Client.GetAsync("http://slowwly.robertomurray.co.uk/delay/8000/url/https://www.api-football.com/demo/").Result;
+
+                                //throw new Exception("errro");
+
+                                //Console.WriteLine($"Result {result.StatusCode} on thread {Thread.CurrentThread.ManagedThreadId}");
+                            }
+
+                            //ServicePointManager.ReusePort = false;
+                            using (HttpClient Client = new HttpClient())
+                            {
+                                Console.WriteLine($"Processing {currentFile} on thread {Thread.CurrentThread.ManagedThreadId}");
+
+                                var result = Client.GetAsync("http://taskerswift.azurewebsites.net/openapi").Result;
+                                if (result.IsSuccessStatusCode)
+                                    result = Client.GetAsync("http://fakerestapi.azurewebsites.net/api/Books").Result;
+                                if (result.IsSuccessStatusCode)
+                                    result = Client.GetAsync("http://andrewgodfroyportfolioapi.azurewebsites.net/api/LinkType").Result;
+                                if (result.IsSuccessStatusCode)
+                                    Client.GetAsync("https://graph.facebook.com");
+
+                                var res = Client.GetAsync("https://www.api-football.com/demo/").Result;
+                                var res2 = Client.GetAsync("http://slowwly.robertomurray.co.uk/delay/8000/url/https://www.api-football.com/demo/").Result;
+
+                                //throw new Exception("errro");
+
+                                //Console.WriteLine($"Result {result.StatusCode} on thread {Thread.CurrentThread.ManagedThreadId}");
+                            }
+
+                        }
+
+                        //throw new Exception("asasas");
 
                     }
                     catch (Exception ex)
                     {
 
-                        Console.WriteLine($"Error  {currentFile} on thread {Thread.CurrentThread.ManagedThreadId}");
+                        //Console.WriteLine($"Error  {currentFile} on thread {Thread.CurrentThread.ManagedThreadId}");
 
-                        exceptions.Enqueue(ex);
+                        if((ex.ToString().IndexOf("de tempo") <=0) && (ex.ToString().IndexOf("cancelamento") <= 0) && (ex.ToString().IndexOf("task was") <= 0))
+                        {
+                            exceptions.Enqueue(ex);
+
+                            if (exceptions.Count > 1)
+                                endProcess = true;
+                        }
+
                     }
-                    
+
 
                 });
+
+                }
 
                 // Throw the exceptions here after the loop completes.
                 //if (exceptions.Count > 0) throw new AggregateException(exceptions);
@@ -80,7 +174,7 @@ namespace TestePortas
             catch (Exception ex)
             {
 
-                Console.WriteLine($"Error  {ex.Message} on program");
+                Console.WriteLine($"Error  {ex.ToString()} on program");
             }
 
 
